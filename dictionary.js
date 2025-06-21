c42e8b1f55a7b152fba40dbf1a0c513bbf26f748
Johannes Fuchs <76202135+fucjoh@users.noreply.github.com>
2025-06-21 16:25:39 +0200

    Add German dictionary and load in JS

---
+++ dictionary.js
@@ -0,0 +1,7 @@
+const germanWords = [
+  "der", "die", "das", "und", "in", "den", "von", "zu", "mit", "sich",
+  "auf", "f\u00fcr", "ist", "im", "dem", "nicht", "ein", "eine", "als", "auch",
+  "es", "an", "werden", "aus", "er", "hat", "sie", "nach", "wird", "bei",
+  "einer", "um", "am", "sind", "noch", "wie", "einem", "\u00fcber", "einen", "so",
+  "zum", "war", "haben", "nur", "oder", "mehr", "wir", "mir", "man", "was"
+];
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 document.getElementById('searchButton').addEventListener('click', function() {
-    const value1 = document.getElementById('field1').value;
-    const value2 = document.getElementById('field2').value;
-    console.log('Suche nach:', value1, value2);
+    const value1 = document.getElementById('field1').value.toLowerCase();
+    const value2 = document.getElementById('field2').value.toLowerCase();
+
+    const results = germanWords.filter(w => w.startsWith(value1) || w.startsWith(value2));
+    console.log('Gefundene Wörter:', results);
 });
+
+console.log('Geladenes Wörterbuch enthält', germanWords.length, 'Wörter');
