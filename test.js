54b4349e562ba2ef65c1aad7da6aa908e87fe638
Johannes Fuchs <76202135+fucjoh@users.noreply.github.com>
2025-06-22 17:42:02 +0200

    Enforce monotonic length changes

--- test.js
+++ test.js
@@ -17,4 +17,20 @@ assert.deepStrictEqual(chain, ['cat', 'cot', 'cog', 'dog']);
 const chain2 = findChain('cat', 'bat', dictionaryByLength);
 assert.strictEqual(chain2, null);
 
+// additional tests for length restrictions
+const words2 = ['a', 'ab', 'ba'];
+const { dictionaryByLength: dict2 } = preprocessDictionary(words2);
+const chain3 = findChain('a', 'ba', dict2);
+assert.deepStrictEqual(chain3, ['a', 'ba']);
+
+const words3 = ['ab', 'a', 'b'];
+const { dictionaryByLength: dict3 } = preprocessDictionary(words3);
+const chain4 = findChain('ab', 'b', dict3);
+assert.deepStrictEqual(chain4, ['ab', 'b']);
+
+const words4 = ['ab', 'a', 'ca', 'cd'];
+const { dictionaryByLength: dict4 } = preprocessDictionary(words4);
+const chain5 = findChain('ab', 'cd', dict4);
+assert.strictEqual(chain5, null);
+
 console.log('All tests passed.');
